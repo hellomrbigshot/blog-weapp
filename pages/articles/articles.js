e8add0ab19b9471217c0c9d6b24cad43a9b8b390
hellomrbigshot <1994yechen@gmail.com>
2018-12-24 18:15:20 +0800

    update artice list page

--- pages/articles/articles.js
+++ pages/articles/articles.js
@@ -1,52 +1,39 @@
 // pages/articles/articles.js
-const util = require('../../utils/util.js')
 const app = getApp();
+const marked = require('../../utils/marked.js');
+const util = require('../../utils/util.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    seach_str: '',
+    search_str: '',
     articles: [],
-    active: 0
+    total: 0,
+    active: 0,
+    search_obj: {
+      pageSize: 5,
+      page: 1,
+      status: 'normal',
+      type: '',
+      secret: false,
+      sort: 'update_date'
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const self = this;
-    wx.request({
-      url: 'https://m.hellomrbigbigshot.xyz/api/page/pagelist',
-      method: 'POST',
-      data: {
-        status: 'normal',
-        secret: false,
-        sort: 'update_date'
-      },
-      header: {
-        'content-type': 'application/x-www-form-urlencoded' 
-      },
-      success: (res) => {
-        if (res.data.code === 'OK') {
-          this.setData({
-            articles: res.data.data.result.map(item => {
-              item.create_date = util.formatTime(item.create_date, '3');
-              item.update_date = util.formatTime(item.update_date, '3');
-              return item;
-            })
-          })
-        }
-      }
-    })
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    this.getList();
   },
 
   /**
@@ -74,14 +61,22 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+    console.log('haha')
+    this.setData({
+      'search_obj.page': 1
+    });
+    this.getList();
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+    if (this.data.search_obj.page * this.data.search_obj.pageSize >= this.data.total) return false;
+    this.setData({
+      'search_obj.page': this.data.search_obj.page + 1
+    });
+    this.getList();
   },
 
   /**
@@ -92,5 +87,38 @@ Page({
   },
   onChange: function () {
     
+  },
+  getList: function () {
+    const self = this;
+    wx.request({
+      url: 'https://m.hellomrbigbigshot.xyz/api/page/pagelist',
+      method: 'POST',
+      data: this.data.search_obj,
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' 
+      },
+      success: (res) => {
+        if (res.data.code === 'OK') {
+          this.setData({
+            'total': res.data.data.total
+          });
+          if (this.data.search_obj.page === 1) {
+            this.setData({
+              'articles': []
+            })
+          }
+          this.setData({
+            'articles': this.data.articles.concat(res.data.data.result.map(item => {
+                item.create_date = util.formatTime(item.create_date, '3');
+                item.update_date = util.formatTime(item.update_date, '3');
+                item.content = marked(item.content).replace(/<[^>]+>/g, '');
+                return item;
+              })
+            )
+          })
+          wx.stopPullDownRefresh();
+        }
+      }
+    })
   }
 })
